b571279f9b6d2902ccfff958a0de7346a7cc549a
nealzc <nealzc1991@gmail.com>
2019-02-23 14:50:20 +0800

    vmtest

--- my_backtest.py
+++ my_backtest.py
@@ -13,8 +13,8 @@ from tqsdk import TqApi, TqSim, TqBacktest, BacktestFinished, TargetPosTask
 
 from trading_selection import my_assets_selection
 from trading_selection import symbols_selection
-from bootstrap_assessment import bootstrap_assessment
-from my_TqBacktest import TianqinBacktesing
+# from bootstrap_assessment import bootstrap_assessment
+# from my_TqBacktest import TianqinBacktesing
 
 pd.set_option('display.max_rows', None)  # 设置Pandas显示的行数
 pd.set_option('display.width', None)  # 设置Pandas显示的宽度
@@ -48,8 +48,7 @@ print('空头合约：', dict_symbols['short'])
 ls_long_symbols = dict_symbols['long']
 ls_short_symbols = dict_symbols['short']
 
-#
-#
+
 # TODO: 交易规则 不同品种不一样。。
 # eg., rb
 long_asset = ls_long_assets[0]
@@ -64,7 +63,7 @@ close_hour_night, close_minute_night = 22, 56
 # based on the trading break: 10:15-10:29, hard to adjust time interval
 duration_seconds = 5 * 60
 # duration_seconds = 15 * 60
-duration_seconds = 24*60*60
+# duration_seconds = 24*60*60
 
 # backtest period
 start_date = dt.date(2018, 7, 2)
@@ -78,3 +77,36 @@ symbol = 'SHFE.rb1810'
 TianqinBacktesing()
 
 # Construct live_monitoring
+
+
+if __name__ == '__main__':
+    from trading_strategies.technical_indicators import SMA
+
+    symbol = 'SHFE.rb1905'
+    duration_seconds = 10
+    # api = TqApi('SIM', url='ws://192.168.1.6:7777', backtest=TqBacktest(start_dt=dt.date(2019,2,13), end_dt=dt.date(2019,2,14)))
+    api = TqApi('SIM', backtest=TqBacktest(start_dt=dt.date(2019, 2, 13), end_dt=dt.date(2019, 2, 14)))
+    quote = api.get_quote(symbol)
+    klines = api.get_kline_serial(symbol, duration_seconds=duration_seconds)  # 日线
+    target_pos = TargetPosTask(api, symbol)
+
+    with closing(api):
+        try:
+            while True:
+                api.wait_update()
+                if api.is_changing(klines):
+                    now = dt.datetime.strptime(quote["datetime"], "%Y-%m-%d %H:%M:%S.%f")  # 当前quote的时间
+                    print(now)
+                    if SMA(klines.close[-50], 20) == 1:  # 如果预测结果为涨: 买入
+                        print(quote["datetime"], "预测下一交易日为 涨")
+                        target_pos.set_target_volume(1)
+                    elif SMA(klines.close[-50], 20) == -1:
+                        print(quote["datetime"], "预测下一交易日为 跌")
+                        target_pos.set_target_volume(-1)
+                    else:
+                        print(quote["datetime"], "meiyoufangxiang pingcang")
+                        target_pos.set_target_volume(0)
+        except BacktestFinished:  # 回测结束, 获取预测结果，统计正确率
+            print("----回测结束----")
+
+
---
+++ trading_strategies/technical_indicators.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+def SMA(ys, k):
+    """
+
+    :param ys: column vector of price series with str time index
+    :param k: lag number
+    :return signal: 1, -1 or 0
+    """
+    SMA = ys.rolling(window=k).apply(np.mean, raw=True)
+    if ys[-1] > SMA[-1] and ys[-2] < SMA[-2]:
+        signal = 1
+    elif ys[-1] < SMA[-1] and ys[-2] > SMA[-2]:
+        signal = -1
+    else:
+        signal = 0
+    return signal
