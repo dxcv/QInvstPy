3a9f23b20693bf99f2d0ed0031f8a76ab3e11f19
nealchenzhang <nealzc1991@gmail.com>
2019-03-08 16:03:54 +0800

    test

--- test/my_strategy1.py
+++ test/my_strategy1.py
@@ -17,10 +17,11 @@ from trading_strategies.technical_indicators import MACD_adj
 SYMBOL = 'SHFE.rb1905'
 CLOSE_HOUR, CLOSE_MINUTE = 14, 50  # 平仓时间
 
-api = TqApi(TqSim(), backtest=TqBacktest(start_dt=dt.date(2019,1,2), end_dt=dt.date(2019,3,5)))
+api = TqApi(TqSim(), backtest=TqBacktest(start_dt=dt.date(2019,1,2), end_dt=dt.date(2019,3,6)))
 # api = TqApi('SIM')
 quote = api.get_quote(SYMBOL)
-klines = api.get_kline_serial(SYMBOL, duration_seconds=15*60)
+klines_15 = api.get_kline_serial(SYMBOL, duration_seconds=15*60)
+klines_1 = api.get_kline_serial(SYMBOL, duration_seconds=60)
 position = api.get_position(SYMBOL)
 target_pos = TargetPosTask(api, SYMBOL)
 target_pos_value = position["volume_long"] - position["volume_short"]  # 净目标净持仓数
@@ -32,38 +33,36 @@ with closing(api):
     while True:
         target_pos.set_target_volume(target_pos_value)
         api.wait_update()
-        if api.is_changing(klines[-1], 'datetime'):
+        if api.is_changing(klines_15[-1], 'datetime'):
 
-            now = dt.datetime.strptime(quote["datetime"], "%Y-%m-%d %H:%M:%S.%f")
-            print(now)
+            k15 = str(dt.datetime.fromtimestamp(klines_15.datetime[-1] / 1e9))
+            print(k15)
 
-            ys = pd.Series(data=klines.close[-40:],
-                           index=[str(dt.datetime.fromtimestamp(i / 1e9)) for i in klines.datetime[-40:]]
+            ys = pd.Series(data=klines_15.close[-40:],
+                           index=[str(dt.datetime.fromtimestamp(i / 1e9)) for i in klines_15.datetime[-40:]]
                            )
             dict_results = MACD_adj(ys)
-
-        if api.is_changing(quote, 'datetime'):
-            # print("最新价: ", quote['last_price'])
-            # TODO 止损平仓逻辑
-            if (target_pos_value > 0 and quote["last_price"] - position["open_price_long"] >= 2) or\
-                    (target_pos_value < 0 and (position["open_price_short"] - quote["last_price"]) >= 2):
-                target_pos_value = 0
-                print('止盈平仓')
 
             if target_pos_value == 0:
                 # 上涨阶段金叉 做多
                 if dict_results['signal'] == 1:
-                    target_pos_value = 1
+                    target_pos_value = 2
                     print("上涨阶段金叉 做多")
                 # 下跌阶段死叉，做空
                 if dict_results['signal'] == -1:
-                    target_pos_value = -1
+                    target_pos_value = -2
                     print("下跌阶段死叉，做空")
 
+            if (target_pos_value > 0 and (dict_results['MACD'][-1] - dict_results['SL'][-1]) <
+                    (dict_results['MACD'][-2] - dict_results['SL'][-2])) or \
+                    (target_pos_value < 0 and (dict_results['MACD'][-1] - dict_results['SL'][-1]) >
+                     (dict_results['MACD'][-2] - dict_results['SL'][-2])):
+                target_pos_value = 0
+                print('止损平仓')
 
-        # if api.is_changing(quote):
-        #     print('浮动盈亏：', account['float_profit'])
-        #     if account['float_profit'] > 200:
-        #         target_pos.set_target_volume(0)
-
-
+        if api.is_changing(quote, 'datetime'):
+            # print("最新价: ", quote['datetime'])
+            if (target_pos_value > 0 and quote["last_price"] - position["open_price_long"] >= 2) or\
+                    (target_pos_value < 0 and (position["open_price_short"] - quote["last_price"]) >= 2):
+                target_pos_value = 0
+                print('止盈平仓')
